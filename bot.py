1284d849423798b9a759216efd18bc492f7e59bc
elfzy <wallisonalmeida935@gmail.com>
2018-02-06 13:52:37 -0200

    arrumando formatação do texto

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from xvideos import choose_random_porn_comment
 bot = commands.Bot(command_prefix='!')
 
 def format_comment(author, content, title):
-    mask = '**{0} comentou o seguinte:**\n`{1}`\n\n**Vi isso no video:**\n`{2}`'
+    mask = '**O {0}  comentou o seguinte:**\n`{1}`\n\n**vi isso no video:**\n`{2}`'
     return mask.format(author, content, title)
 
 
@@ -18,7 +18,7 @@ async def on_ready():
 
 @bot.command(description='Procura um comentário no xvideos.')
 async def comentario():
-    await bot.say('**Vou procurar um comentario aqui, perae...**')
+    await bot.say('**Vou procurar um comentario aqui, perae...\n**')
 
     try:
         comment = choose_random_porn_comment()
