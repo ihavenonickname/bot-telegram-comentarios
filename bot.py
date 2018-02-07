93e2fceecd0f081ef61286962ffcaf86232b0970
marquesgabriel <gabrielf.pmarques@gmail.com>
2018-02-07 08:20:12 -0200

    exploring possibilities

--- bot.py
+++ bot.py
@@ -6,25 +6,25 @@ from xvideos import choose_random_porn_comment
 bot = commands.Bot(command_prefix='!')
 
 def format_comment(author, content, title):
-    mask = '**O {0}  comentou o seguinte:**\n`{1}`\n\n**vi isso no video:**\n`{2}`'
+    mask = '**O {0}  comentou o seguinte:**\n`{1}`\n**'
     return mask.format(author, content, title)
 
 
 @bot.event
 async def on_ready():
     print("Bot online!")
-    await bot.change_presence(game=discord.Game(name='!comentario'))
+    await bot.change_presence(game=discord.Game(name='digite !recadinho ou !telemensagem'))
 
 
 @bot.command(description='Procura um comentário no xvideos.')
 async def comentario():
-    await bot.say('**Vou procurar um comentario aqui, perae...\n**')
+    await bot.say('**Psicografando...\n**')
 
     try:
         comment = choose_random_porn_comment()
         await bot.say(format_comment(*comment))
     except Exception:
-        bot.say('Houve um erro! fala com o dev dessa caralha.')
+        bot.say('Houve uma falha no broadcast divino.')
 
 
 bot.run('TOKEN')
