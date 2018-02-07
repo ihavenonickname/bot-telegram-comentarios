24ce4fdd71de2b4117bcc9cbdff5d175f5ec4354
marquesgabriel <gabrielf.pmarques@gmail.com>
2018-02-07 12:48:57 -0200

    testing TTS

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 def format_comment(author, content, title):
-    mask = '**O {0}  comentou o seguinte:**\n`{1}`\n**'
+    mask = '**O {0}  comentou o seguinte:**\n`{1}`\n'
     return mask.format(author, content, title)
 
 
@@ -19,12 +19,22 @@ async def on_ready():
 
 
 @bot.command(description='Procura um comentário no xvideos.')
-async def comentario():
+async def recadinho():
     await bot.say('**Psicografando...\n**')
 
     try:
         comment = choose_random_porn_comment()
         await bot.say(format_comment(*comment))
+    except Exception:
+        bot.say('Houve uma falha no broadcast divino.')
+
+@bot.command(description='Procura um comentário no xvideos.')
+async def telemensagem():
+    await bot.say('**Psicografando audio...\n**')
+
+    try:
+        comment = choose_random_porn_comment()
+        await bot.say(format_comment(*comment), tts=True)
     except Exception:
         bot.say('Houve uma falha no broadcast divino.')
 
