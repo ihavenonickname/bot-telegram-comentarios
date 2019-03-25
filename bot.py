b9b8d2e23b4f5973b523debc4d61c634699f384a
marquesgabriel <gabrielf.pmarques@gmail.com>
2019-03-25 13:00:56 -0300

    rollback para ultima versão funcional

--- bot.py
+++ bot.py
@@ -45,14 +45,11 @@ async def telemensagem():
         bot.say('Houve uma falha na busca. Tente novamente.')
 
 
-@bot.command(description='Procura um video baseado na tag passada.', pass_context=True)
-async def busca(ctx, tag=None):
-    print(ctx.message.author)
-    # print(tag)
+@bot.command(description='Procura um video baseado na tag passada.')
+async def busca(tag=None):
     try:
         link = choose_random_video(tag)
-        # await bot.say('Segura esse link aí: ' + link)
-        await ctx.message.author.send(str('Segura esse link aí: ' + link))
+        await bot.say('Segura esse link aí: ' + link)
     except Exception:
         bot.say('Houve uma falha na busca. Tente novamente.')
 
